89f66783873c30e0c537790aea6ddde32728c1f4
Ahmed Shafique <99950606+AhmedShafique313@users.noreply.github.com>
2025-03-14 19:07:14 +0500

    still working

--- extraction.py
+++ extraction.py
@@ -1,7 +1,7 @@
 import cv2
 import pytesseract
 
-image_path = r"C:\Users\Infinity AI Systems\Documents\Projects\AI OCR System for Invoices\image\invoice_1.jpg"  # Change this to your uploaded image path
+image_path = r"C:\Users\Infinity AI Systems\Documents\Projects\AI OCR System for Invoices\image\invoice_2.jpg"  # Change this to your uploaded image path
 image = cv2.imread(image_path)
 
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
