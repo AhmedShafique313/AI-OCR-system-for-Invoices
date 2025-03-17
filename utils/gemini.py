23906629591bb1849325cfc43626a1e0b5e28e31
Ahmed Shafique <99950606+AhmedShafique313@users.noreply.github.com>
2025-03-17 22:33:15 +0500

    fixing the prompt engineering

--- utils/gemini.py
+++ utils/gemini.py
@@ -13,18 +13,15 @@ llm = ChatGoogleGenerativeAI(
 def process_with_gemini(extracted_text):
     prompt = f""" THE EXTRACTED TEXT IS THE TEXT OF AN INVOCE
     Extract the following structured data from the OCR text,The Terms can be capital or not you have to figure it out yourself:
-    - INVOICE (Its shows invoice number it is also not case sensitive)
-    - DATE CREATED
-    - VENDOR (company name)
-    - SALE TYPE
-    - DELIVER TO
+    - INVOICE NUMBER (Its shows invoice number)
+    - DATE CREATED (On which the invoice created)
+    - VENDOR (It should be complete company name)
+    - SALE TYPE (It should be the part number only)
     - DESCRIPTION (as a list, should be small and concise)
     - Quantity (It is the quantity of the product do not confuse it with any price value, it can  start fron QTY or Qty and may or may not have SHP that is quantity of the product its an integer)
     - UNIT PRICE (It is the price of one unit, It can be replaced by its synonym and its not case sensitive)
     - EXTD PRICE or Total (It can be replaced by its synonym and its not case sensitive)
-    Return the data in **valid JSON format**.
-    Ensure that the response contains only the extracted data as a JSON object.
-
+    Return the data in such format that it can be store in the csv file.
     OCR Text:
     {extracted_text}
     """
