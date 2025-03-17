205b1ab7ef6fff44b1c4d5d382a030d516fdfe0e
Ahmed Shafique <99950606+AhmedShafique313@users.noreply.github.com>
2025-03-17 21:39:44 +0500

    done

--- utils/ocr.py
+++ utils/ocr.py
@@ -7,4 +7,4 @@ def extract_text(file_path):
     extracted_text = [text[1] for text in results]
     return print(extracted_text)
 
-extract_text(file_path= r'C:\Users\Infinity AI Systems\Documents\Projects\AI OCR System for Invoices\uploads\invoice_2.png')
+# extract_text(file_path= r'C:\Users\Infinity AI Systems\Documents\Projects\AI OCR System for Invoices\uploads\invoice_2.png')
