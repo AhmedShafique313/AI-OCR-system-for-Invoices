1ae79f278a421d9481f3ee800015b1c92cf92cee
Ahmed Shafique <99950606+AhmedShafique313@users.noreply.github.com>
2025-03-17 21:40:00 +0500

    good

--- utils/ocr.py
+++ utils/ocr.py
@@ -5,6 +5,6 @@ reader = easyocr.Reader(['en'])
 def extract_text(file_path):
     results = reader.readtext(file_path)
     extracted_text = [text[1] for text in results]
-    return print(extracted_text)
+    return extracted_text
 
 # extract_text(file_path= r'C:\Users\Infinity AI Systems\Documents\Projects\AI OCR System for Invoices\uploads\invoice_2.png')
